528d212a32581b9f5edd151a1e1912f6f19cde5e
nesim-savga <Nesim.savga@my-aolcc.com>
2025-02-13 12:57:55 +0300

    EFApp
    EFApp

---
+++ EFApp/Student.cs
@@ -0,0 +1,21 @@
+namespace EFApp
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class Student
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string LastName { get; set; }
+    }
+}
