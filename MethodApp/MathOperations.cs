115034c50f4cfea0eb29daecfa8e102ad465f6f1
nesim-savga <Nesim.savga@my-aolcc.com>
2025-02-13 12:40:58 +0300

    MethodApp
    MethodApp

---
+++ MethodApp/MathOperations.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MethodApp
+{
+    public class MathOperations
+    {
+        // Create a void method that takes two integers as parameters
+        public void PerformOperation(int num1, int num2)
+        {
+            // Perform a simple math operation on the first integer. Add 5 to the number.
+            double result = num1 + 5;
+
+            // Display the result of the math operation
+            Console.WriteLine("Add 5 to the number");
+            Console.WriteLine("The result of the math operation on the first number is: " + result);
+
+            // Display the second integer
+            Console.WriteLine("The second number is: " + num2);
+        }
+    }
+}
---
+++ MethodApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MethodApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate the MathOperations class
+            MathOperations mathOperations = new MathOperations();
+
+            // Call the PerformOperation method, passing in two numbers
+            mathOperations.PerformOperation(8, 9);
+
+            // Call the PerformOperation method again, specifying the parameters by name
+            mathOperations.PerformOperation(num1: 2, num2: 3);
+
+            // Keep the console window open to see the results
+            Console.ReadLine();
+        }
+    }
+}
